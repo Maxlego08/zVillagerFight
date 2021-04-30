027dbc1c1ba019a49ac6f422a57df37b9867f749
Maxlego08 <maxlego08@sfr.fr>
2021-04-30 21:59:16 +0200

    Update message

--- src/fr/maxlego08/mobfighter/api/enums/Message.java
+++ src/fr/maxlego08/mobfighter/api/enums/Message.java
@@ -60,7 +60,7 @@ public enum Message {
 	COMMAND_DESCRIPTION_ARENA_CENTER("Set arena center location"), 
 	
 	DUEL_START("§fStart of the duel between §a%first% §fand §2%second%§f. May the best man win!"),
-	DUEL_WIN("§a%winner% §f comes to win the fight against §b%looser%§f."),
+	DUEL_WIN("§a%winner% §fjust won the fight against §b%looser%§f."),
 	
 	
 	;
